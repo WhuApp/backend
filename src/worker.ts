f96d0b38dab9634c6a264ab2affcdab88287882e
Kai Jellinghaus <contact@kaij.tech>
2023-08-16 06:53:48 +0200

    Add Friend v1 to Root
    Co-authored-by: Noah Gerber <noahgerber100@gmail.com>

--- src/worker.ts
+++ src/worker.ts
@@ -1,8 +1,18 @@
 import { Env } from "./types";
+import friendV1Fetch from "./services/friend_v1";
 
 export default <ExportedHandler<Env>> {
   async fetch(request, env): Promise<Response> {
     const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/friends/v1/")) {
+      return friendV1Fetch(
+        request,
+        env,
+        url.pathname.substring("/friends/v1/".length),
+      );
+    }
+
     throw new Error();
   },
 };
