bd5ebd358bb71d0e181669bd6ff3af4d6806e1ef
Kai Jellinghaus <contact@kaij.tech>
2023-08-16 21:48:09 +0200

    :sparkles: Friend V1 (#4)
    * Add Back Friends Logic
    
    Co-authored-by: Noah Gerber <noahgerber100@gmail.com>
    Co-authored-by: Nils Osswald <imrebux@gmail.com>
    
    * Add Friend v1 to Root
    
    Co-authored-by: Noah Gerber <noahgerber100@gmail.com>
    
    ---------
    
    Co-authored-by: Noah Gerber <noahgerber100@gmail.com>
    Co-authored-by: Nils Osswald <imrebux@gmail.com>

---
+++ src/auth0.ts
@@ -0,0 +1,46 @@
+import { Env } from "./types";
+
+type Auth0TokenResponse = {
+  token_type: string;
+  access_token: string;
+};
+
+export const userExists = async (id: string, env: Env): Promise<boolean> => {
+  const response = await fetch("https://whuapp.eu.auth0.com/oauth/token", {
+    method: "POST",
+    headers: { "content-type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: env.AUTH0_CLIENT_ID,
+      client_secret: env.AUTH0_TOKEN,
+      audience: "https://whuapp.eu.auth0.com/api/v2/",
+    }),
+  });
+  const body: Auth0TokenResponse = await response.json();
+
+  const headers = new Headers();
+  headers.append("Accept", "application/json");
+  headers.append("Authorization", `${body.token_type} ${body.access_token}`);
+
+  const requestOptions = {
+    method: "GET",
+    headers: headers,
+    redirect: "follow",
+  };
+
+  const auth0Response = await fetch(
+    `https://whuapp.eu.auth0.com/api/v2/users/${id}`,
+    requestOptions,
+  );
+
+  switch (auth0Response.status) {
+    case 200:
+      return true;
+    case 404:
+      return false;
+    case 429:
+      throw new Error("Auth0 Ratelimit");
+    default:
+      throw new Error(`Auth0 error: ${await auth0Response.text()}`);
+  }
+};
