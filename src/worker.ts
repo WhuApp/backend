d19897d7f77a07939c38fd0964a75ab19e91f3ec
Kai Jellinghaus <contact@kaij.tech>
2023-08-16 07:00:51 +0200

    Reformat using Prettier

--- src/worker.ts
+++ src/worker.ts
@@ -1,6 +1,6 @@
-import { Env } from "./types";
+import { Env } from './types';
 
-export default <ExportedHandler<Env>> {
+export default <ExportedHandler<Env>>{
   async fetch(request, env): Promise<Response> {
     const url = new URL(request.url);
     throw new Error();
