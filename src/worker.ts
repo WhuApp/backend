85981bfd23eb262fd8a8a5750d336849bb0cdaed
Nils <ossw.nils@gmail.com>
2023-08-17 10:52:07 +0000

    :sparkles: Add /users/v1 to worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,16 +1,17 @@
-import { Env } from "./types";
-import friendV1Fetch from "./services/friend_v1";
+import { Env } from './types';
+import friendsFetch from './services/friend_v1';
+import usersFetch from './services/users_v1';
 
 export default <ExportedHandler<Env>>{
   async fetch(request, env): Promise<Response> {
     const url = new URL(request.url);
 
-    if (url.pathname.startsWith("/friends/v1/")) {
-      return friendV1Fetch(
-        request,
-        env,
-        url.pathname.substring("/friends/v1/".length),
-      );
+    // TODO: Switch statement & endpoint constants
+    if (url.pathname.startsWith('/friends/v1/')) {
+      return friendsFetch(request, env, url.pathname.substring('/friends/v1/'.length));
+    }
+    if (url.pathname.startsWith('/users/v1/')) {
+      return usersFetch(request, env, url.pathname.substring('/users/v1/'.length));
     }
 
     throw new Error();
