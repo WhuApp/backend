01175061ff7abe4aedd2506839bb85c7d3c14960
Kai Jellinghaus <contact@kaij.tech>
2023-08-19 00:00:55 +0200

    Handle Errors

--- src/worker.ts
+++ src/worker.ts
@@ -4,16 +4,20 @@ import usersFetch from './services/users_v1';
 
 export default <ExportedHandler<Env>>{
   async fetch(request, env): Promise<Response> {
-    const url = new URL(request.url);
+    try{
+      const url = new URL(request.url);
 
-    // TODO: Switch statement & endpoint constants
-    if (url.pathname.startsWith('/friends/v1/')) {
-      return friendsFetch(request, env, url.pathname.substring('/friends/v1/'.length));
-    }
-    if (url.pathname.startsWith('/users/v1/')) {
-      return usersFetch(request, env, url.pathname.substring('/users/v1/'.length));
-    }
+      // TODO: Switch statement & endpoint constants
+      if (url.pathname.startsWith('/friends/v1/')) {
+        return friendsFetch(request, env, url.pathname.substring('/friends/v1/'.length));
+      }
+      if (url.pathname.startsWith('/users/v1/')) {
+        return usersFetch(request, env, url.pathname.substring('/users/v1/'.length));
+      }
 
-    throw new Error();
+      throw new Error();
+    } catch (error) {
+      return new Response(JSON.stringify(error), { status: 500});
+    }
   },
 };
