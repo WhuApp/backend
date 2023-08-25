ee89bf3b58947c8cdbd5f51e3af48a9c3c0fd97c
Nils <imrebux@gmail.com>
2023-08-25 13:18:11 +0200

    Simplify

--- src/worker.ts
+++ src/worker.ts
@@ -5,16 +5,14 @@ export default <ExportedHandler<Env>>{
   async fetch(request, env): Promise<Response> {
     try {
       const url = new URL(request.url);
+      const servicePath = `/${url.pathname.split("/").slice(1, 3).join("/")}/`
 
-      const path: string[] = url.pathname.split('/').slice(1);
-      const pathService: string = `/${path[0]}/${path[1]}/`;
-
-      const activeService = Object.values(services).filter(
-        (service: Service) => service.path === pathService
+      const found = Object.values(services).filter(
+        (service: Service) => service.path === servicePath
       )[0];
 
-      if (activeService) {
-        return await activeService.fetch(request, env, url.pathname.substring(pathService.length));
+      if (found) {
+        return await found.fetch(request, env, url.pathname.substring(servicePath.length));
       } else {
         throw new Error('Service not found');
       }
