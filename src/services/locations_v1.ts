2ea19abc99f8607eb16a5cce4de53ba9b743c131
Nils Osswald <contact@nilsosswald.de>
2023-10-12 23:19:50 +0200

    :poop: Fuck timestamp validation

--- src/services/locations_v1.ts
+++ src/services/locations_v1.ts
@@ -71,11 +71,10 @@ const storeData = async (request: LocationRequest, env: Env): Promise<Response> 
     throw new Response('Data wrong format', { status: 400 });
   }
 
-  const now = Date.now();
-
-  if (location.timestamp > now) {
-    throw new Response(`Invalid date: Got ${location.timestamp}, now: ${now}`, { status: 400 });
-  }
+  // const now = Date.now();
+  // if (location.timestamp > now) {
+  //   throw new Response(`Invalid date: Got ${location.timestamp}, now: ${now}`, { status: 400 });
+  // }
 
   //cut incomming data
   const kvData: TimedLocation = {
@@ -87,7 +86,7 @@ const storeData = async (request: LocationRequest, env: Env): Promise<Response> 
 
   await env.LOCATION_KV.put(id, JSON.stringify(kvData));
 
-  return new Response(`Got ${location.timestamp}, now: ${now}`, { status: 201 });
+  return new Response(null, { status: 201 });
 };
 
 const dataById = async (id: string, env: Env): Promise<Response> => {
